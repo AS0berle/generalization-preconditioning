c6b9861b4231fa58de73f15ee1a28fde626753d1
Alex <aoberle1@umbc.edu>
2024-02-11 00:17:53 -0500

    Move more utility functions out of notebook

--- direct/direct_helpers.py
+++ direct/direct_helpers.py
@@ -1,7 +1,17 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+import torchvision
+from torchvision.datasets import MNIST
+from scipy.optimize import linprog
+
+
+# Magic numbers from the internet
+MNIST_MEAN = 0.1307
+MNIST_STD = 0.3081
+
 
 class ReluCount(nn.ReLU):
     """
@@ -62,6 +72,7 @@ class MnistReluCountModel(nn.Module):
         """
         self.relu_count.reset_count()
     
+
 def count_relu_0s(dataloader: DataLoader, model: MnistReluCountModel):
     model.eval()
     model.begin_count()
@@ -70,3 +81,42 @@ def count_relu_0s(dataloader: DataLoader, model: MnistReluCountModel):
     num_zeros = model.get_activation_count()
     model.reset_count()
     return num_zeros
+
+
+def get_avg_MNIST(sample_size:int=1000, loader:DataLoader=None):
+    if loader is None:
+        loader = DataLoader(MNIST('images/', train=True, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize((MNIST_MEAN,), (MNIST_STD,))
+                                   ])),
+                              batch_size=1, shuffle=True, pin_memory=False)
+    avg = None
+    for sample_num, (X, y) in enumerate(loader):
+        if avg is None:
+            avg = X
+        else:
+            avg = avg + X
+        if sample_num == sample_size:
+            break
+    return avg / sample_num
+
+
+def linprog_parameter(c_values, in_size):
+    # supply out_size different values of c, each randomly sampled
+    # for later matrices, mult in_size diff values through
+    results = []
+    for c in c_values:
+        
+        A_eq = torch.ones_like(c).expand((1, in_size)).detach().numpy()
+        b_eq = torch.zeros((1,)).detach().numpy()
+        c_np = c.detach().numpy()
+        result = linprog(c_np, A_eq=A_eq, b_eq=b_eq, bounds=(-(1 / math.sqrt(in_size)), (1 / math.sqrt(in_size))))
+
+        results.append(result.x)
+
+    result_matrix = torch.stack(
+        [torch.zeros(in_size).uniform_() * torch.tensor(x) for x in results]
+    )
+    
+    return torch.nn.Parameter(result_matrix.to(dtype=torch.float32))
